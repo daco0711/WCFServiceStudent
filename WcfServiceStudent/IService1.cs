234839c517244fb7e0296f4c770a46a4bcff9346
Danijel Filipovic <daco0711@hotmail.com>
2014-12-02 15:41:52 +0100

    Inicijalni komit

---
+++ WcfServiceStudent/IService1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceStudent
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(Namespace = "WcfServiceStudent")]
+    public interface IService1
+    {
+
+        [OperationContract(Action = "StudentService/GetStudents")]
+        List<Student> getStudents();
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract(Action = "StudentService/GetStudentById")]
+        Student getStudentById(int studentID);
+
+        [OperationContract(Action = "StudentService/deleteStudent")]
+        void deleteStudent(Int32 studentID);
+
+        [OperationContract(Action = "StudentService/AddStudent")]
+        void addStudent(String studentName,String IndexNumber, String City, String address, String jmbg, String sex, DateTime datumRodj );
+
+        [OperationContract(Action = "StudentService/updateStudent")]
+        void updateStudent(Int32 studentID, String studentName, String IndexNumber, String City, String address, String jmbg, String sex, DateTime datumRodj);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract(Namespace = "WcfServiceStudent")]
+    public class Student
+    {
+        public Int32 studentId;
+        public String studentName;
+        public String indexNumber;
+        public String city;
+        public String address;
+        public String JMBG;
+        public String sex;
+        public DateTime birthDate;
+
+        [DataMember]
+        public Int32 StudentId
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+
+        [DataMember]
+        public String StudentName
+        {
+            get { return studentName; }
+            set { studentName = value; }
+        }
+
+        [DataMember]
+        public String IndexNumber
+        {
+            get { return indexNumber; }
+            set { indexNumber = value; }
+        }
+
+        [DataMember]
+        public String City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        [DataMember]
+        public String Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        [DataMember]
+        public String Jmbg
+        {
+            get { return JMBG; }
+            set { JMBG = value; }
+        }
+
+        [DataMember]
+        public String Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
+
+        [DataMember]
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+
+
+
+
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WcfServiceStudent/Service1.svc.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WcfServiceStudent
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public List<Student> getStudents()
+        {
+            SqlConnection connection = new SqlConnection();
+            DataSet dataset = new DataSet();
+            SqlDataAdapter dataadapter = new SqlDataAdapter();
+            SqlCommand query = new SqlCommand("Select * from tbl_Student");
+            connection.ConnectionString = ("Data Source=DACO-PC;Initial Catalog=Student;User ID=sa;Password = daco;MultipleActiveResultSets=True");
+            query.Connection = connection;
+            connection.Open();
+            SqlDataReader dataReader = query.ExecuteReader();
+            List<Student> students = new List<Student>();
+
+            while (dataReader.Read())
+            {
+                Student student = new Student();
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.StudentId = Convert.ToInt32(dataReader["ID"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.StudentName = Convert.ToString(dataReader["StudentName"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.IndexNumber = Convert.ToString(dataReader["IndexNumber"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.City = Convert.ToString(dataReader["City"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.Address = Convert.ToString(dataReader["Address"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.studentId = Convert.ToInt32(dataReader["ID"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.Jmbg = Convert.ToString(dataReader["JMBG"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.Sex = Convert.ToString(dataReader["Sex"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.BirthDate = Convert.ToDateTime(dataReader["BirthDate"]);
+                }
+                students.Add(student);
+
+
+            }
+            return students;
+
+
+
+        }
+
+        public Student getStudentById(int studentID)
+        {
+            SqlConnection connection = new SqlConnection();
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            SqlCommand query = new SqlCommand("select * from tbl_Student where ID=" + studentID);
+            connection.ConnectionString = "Data Source=DACO-PC;Initial Catalog=Student;User ID=sa;Password = daco;MultipleActiveResultSets=True";
+            query.Connection = connection;
+
+            connection.Open();
+            SqlDataReader dataReader = query.ExecuteReader();
+
+            Student student = new Student();
+            while (dataReader.Read())
+            {
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.StudentId = Convert.ToInt32(dataReader["ID"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.StudentName = Convert.ToString(dataReader["StudentName"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.IndexNumber = Convert.ToString(dataReader["IndexNumber"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.City = Convert.ToString(dataReader["City"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.Address = Convert.ToString(dataReader["Address"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.studentId = Convert.ToInt32(dataReader["ID"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.Jmbg = Convert.ToString(dataReader["JMBG"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.Sex = Convert.ToString(dataReader["Sex"]);
+                }
+
+                if (!Convert.IsDBNull(dataReader["ID"]))
+                {
+                    student.BirthDate = Convert.ToDateTime(dataReader["BirthDate"]);
+                }
+            }
+            dataReader.Close();
+            return student;
+        }
+
+        public void deleteStudent(Int32 studentID)
+        {
+
+            Student delStud = new Student();
+            SqlConnection connection = new SqlConnection();
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            SqlCommand query = new SqlCommand("delete from tbl_Student where ID=" + studentID);
+            connection.ConnectionString = "Data Source=DACO-PC;Initial Catalog=Student;User ID=sa;Password = daco;MultipleActiveResultSets=True";
+            query.Connection = connection;
+            connection.Open();
+            SqlDataReader dataReader = query.ExecuteReader();
+            dataReader.Close();
+        }
+
+        public void addStudent(String studentName, String IndexNumber, String City, String address, String jmbg, String sex, DateTime datum)
+        {
+            Student dodStud = new Student();
+
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Data Source=DACO-PC;Initial Catalog=Student;User ID=sa;Password = daco;MultipleActiveResultSets=True";
+            string qString = " Insert into tbl_Student (studentName,IndexNumber,City,Address,JMBG,Sex,BirthDate) values (@studentName,@IndexNUmber,@City,@Address,@JMBG,@Sex,@datum)";
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = qString;
+            cmd.Connection = connection;
+
+
+            cmd.Parameters.Add("@studentName", SqlDbType.VarChar);
+            cmd.Parameters["@studentName"].Value = studentName;
+
+            cmd.Parameters.Add("@IndexNumber", SqlDbType.VarChar);
+            cmd.Parameters["@IndexNumber"].Value = IndexNumber;
+
+            cmd.Parameters.Add("@City", SqlDbType.VarChar);
+            cmd.Parameters["@City"].Value = City;
+
+            cmd.Parameters.Add("@Address", SqlDbType.VarChar);
+            cmd.Parameters["@Address"].Value = address;
+
+            cmd.Parameters.Add("@JMBG", SqlDbType.VarChar);
+            cmd.Parameters["@JMBG"].Value = jmbg;
+
+            cmd.Parameters.Add("@Sex", SqlDbType.VarChar);
+            cmd.Parameters["@Sex"].Value = sex;
+
+            cmd.Parameters.Add("@datum", SqlDbType.DateTime);
+            cmd.Parameters["@datum"].Value = datum;
+
+            try
+            {
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+
+        }
+        public void updateStudent(Int32 studentID, String studentName, String IndexNumber, String City, String address, String jmbg, String sex, DateTime datumRodj)
+        {
+
+            SqlConnection connection = new SqlConnection();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+            String qString = ("update tbl_Student set studentName=@studentName,IndexNumber=@IndexNumber,City=@City,Address=@Address,JMBG=@JMBG,Sex=@Sex,BirthDate=@BirthDate where ID=@studentID");
+            connection.ConnectionString = "Data Source=DACO-PC;Initial Catalog=Student;User ID=sa;Password = daco;MultipleActiveResultSets=True";
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = qString;
+            cmd.Connection = connection;
+
+            cmd.Parameters.Add("@studentName", SqlDbType.VarChar);
+            cmd.Parameters["@studentName"].Value = studentName;
+
+            cmd.Parameters.Add("@IndexNumber", SqlDbType.VarChar);
+            cmd.Parameters["@IndexNumber"].Value = IndexNumber;
+
+            cmd.Parameters.Add("@City", SqlDbType.VarChar);
+            cmd.Parameters["@City"].Value = City;
+
+            cmd.Parameters.Add("@Address", SqlDbType.VarChar);
+            cmd.Parameters["@Address"].Value = address;
+
+            cmd.Parameters.Add("@JMBG", SqlDbType.VarChar);
+            cmd.Parameters["@JMBG"].Value = jmbg;
+
+            cmd.Parameters.Add("@Sex", SqlDbType.VarChar);
+            cmd.Parameters["@Sex"].Value = sex;
+
+            cmd.Parameters.Add("@BirthDate", SqlDbType.DateTime);
+            cmd.Parameters["@BirthDate"].Value = datumRodj;
+
+            cmd.Parameters.Add("@studentID", SqlDbType.Int);
+            cmd.Parameters["@studentID"].Value = studentID;
+
+            try
+            {
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                cmd.Connection.Close();
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+        
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
